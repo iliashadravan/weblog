f11cccea9fcd780093ced604deb596e3a4ce3862
Your Name <you@example.com>
2024-08-10 10:55:37 +0330

    check is_admin

--- controller/articles_after.login.php
+++ controller/articles_after.login.php
@@ -11,14 +11,23 @@ if (!isset($_SESSION['email'])) {
 
 $email = $_SESSION['email'];
 
+// بررسی نقش کاربر (آیا ادمین است یا خیر)
+$sql = "SELECT * FROM `users` WHERE email='$email'";
+$result = $db->query($sql);
+$user = $result->fetch_assoc();
+
+if ($user['is_admin'] == 1) {
+    header('Location:/weblog/view/goback.html');
+    exit();
+
+
 // استخراج تمامی مقالات از پایگاه داده
-$sql = "SELECT blog.id, blog.title, blog.body, blog.time, users.firstname, users.lastname, users.email AS user_email 
+    $sql = "SELECT blog.id, blog.title, blog.body, blog.time, users.firstname, users.lastname, users.email AS user_email 
         FROM blog 
         JOIN users ON blog.user_id = users.id";
-$result = $db->query($sql);
+    $result = $db->query($sql);
 
-if ($result === false) {
-    die("Error: " . $db->error);
+    if ($result === false) {
+        die("Error: " . $db->error);
+    }
 }
-
-?>
--- controller/user/save_article.php
+++ controller/user/save_article.php
@@ -3,11 +3,28 @@ global $db;
 require_once '../../controller/db.php';
 require_once '../../controller/functions.php';
 
+session_start();  // اضافه کردن session_start() برای اطمینان از استفاده از $_SESSION
 
 $errors = [];
 
+if (!isset($_SESSION['email'])) {
+    header('Location: /weblog/view/login.php');
+    exit();
+}
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {     // POST is secretary than GET
+$email = $_SESSION['email'];
+
+// بررسی نقش کاربر (آیا ادمین است یا خیر)
+$sql = "SELECT * FROM `users` WHERE email='$email'";
+$result = $db->query($sql);
+$user = $result->fetch_assoc();
+
+if ($user['is_admin'] == 1) {
+    header('Location:/weblog/view/goback.html');
+    exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {     // POST is more secure than GET
     $title = request('title');
     $body = request('body');
 
@@ -17,23 +34,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {     // POST is secretary than GET
     if (is_null($body)) {
         $errors['body'] = 'body is empty';
     }
-    $sql="SELECT * FROM `users` WHERE email='{$_SESSION['email']}'";
-    $result=$db->query($sql);
-    $user = $result -> fetch_assoc();
+
     $user_id = $user['id'];
+
     if (!is_null($title) && !is_null($body)) {
 
         date_default_timezone_set('Asia/Tehran');         // تنظیم ساعت بر تهران
-        $time=date('Y-m-d H:i:s');       // قسمت ذخیره ساعت و تاریخ
-        $SQL = "insert into `blog` (title, body,user_id,time) values ('$title', '$body','$user_id' ,'$time') ";
+        $time = date('Y-m-d H:i:s');       // قسمت ذخیره ساعت و تاریخ
+        $SQL = "INSERT INTO `blog` (title, body, user_id, time) VALUES ('$title', '$body', '$user_id', '$time')";
+
         if ($result = mysqli_query($db, $SQL)) {
             $success = true;
         } else {
             echo 'error: ' . mysqli_error($db);
             exit;
         }
-        header("location:/weblog/view/user/index.php");
+
+        header("Location: /weblog/view/user/index.php");
+        exit();
     }
 }
-
 ?>
